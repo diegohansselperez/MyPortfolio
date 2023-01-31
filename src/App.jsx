277f072e063fc856819962bcd404f452e9377b41
Diego Hanssel Cabrales <diegohansselp24@gmail.com>
2023-01-31 10:39:20 -0700

    cambios en el router de los archivos App.jsx e index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
-import { AboutMe } from "./pages/aboutMe";
-
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/aboutme" element={<AboutMe />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { Contact } from "./Contact";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -6,7 +7,9 @@ import { Skills } from "./Skills";
 
 export function Home() {
     return (
-       <>
+       <>   <Routes>
+                <Route path="/aboutme" element={<AboutMe />} />
+            </Routes>
             <Header />
             <Skills />
             <Proyectos />
