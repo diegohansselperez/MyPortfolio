7e5f0d11f83053c6f75246ee01b0efc4588e3495
diego hanssel perez <diegohansselp24@gmail.com>
2024-03-26 23:40:10 -0700

    modified:   src/components/Footer.jsx     modified:   src/components/Proyectos.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,17 @@ import { SocialItems } from './Submodules';
 
 export function Footer() {
   return (
-    <>
-      <footer className={style.footer}>
-        <div className={style.footer_content}>
-          <h5>Diego Hanssel Cabrales Perez</h5>
-          <h5>Desarrollador Web Full-Stack</h5>
-        </div>
-        <SocialItems />
-        <div>
-          <Link className={style.hacia_arriba} to="/#home_nav" onClick>
-            Ir al Comienzo
-          </Link>
-        </div>
-      </footer>
-    </>
+    <footer className={style.footer}>
+      <div className={style.footer_content}>
+        <h5>Diego Hanssel Cabrales Perez</h5>
+        <h5>Desarrollador Web Full-Stack</h5>
+      </div>
+      <SocialItems />
+      <div>
+        <Link className={style.hacia_arriba} to="/#home_nav" onClick>
+          Ir al Comienzo
+        </Link>
+      </div>
+    </footer>
   );
 }
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -101,6 +101,4 @@ export function Proyectos() {
       </div>
     </div>
   );
-  );
 }
-
