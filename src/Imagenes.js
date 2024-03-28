70c42942e13cf84993c192352388d4ac3b9a4829
diego hanssel perez <diegohansselp24@gmail.com>
2024-03-28 11:53:36 -0700

    Solucionamos error en package.json

--- src/Imagenes.js
+++ src/Imagenes.js
@@ -1,38 +1,33 @@
-import LiteraryCorner from '../src/assets/img/LiteraryCorner.png'
-import YearSale from '../src/assets/img/captura_Yearsale.png'
-import Homework from '../src/assets/img/captura_homework-list.png'
-import BadBear from '../src/assets/img/captura_bear_bad_studio.png'
-import MiPortfolio from '../src/assets/img/captura_portfolio(1).png'
-import PI_Henry_Videogames from "../src/assets/img/PI_Henry.png"
-import linkedin from "../src/assets/icons/linkedin.png"
+import LiteraryCorner from '../src/assets/img/LiteraryCorner.png';
+import YearSale from '../src/assets/img/captura_Yearsale.png';
+import Homework from '../src/assets/img/captura_homework-list.png';
+import BadBear from '../src/assets/img/captura_bear_bad_studio.png';
+import MiPortfolio from '../src/assets/img/captura_portfolio(1).png';
+import PI_Henry_Videogames from '../src/assets/img/PI_Henry.png';
 
 export default [
-    {
-        "img1":LiteraryCorner,
-        "alt":"LiteraryCorner"
-    }
-    ,
-    {
-        "img2":YearSale,
-        "alt":"YearSale"
-    },
-    {
-        "img3":Homework,
-        "alt":"Homework_list"
-    },
-    {
-        "img4":BadBear,
-        "alt":"BadBear"
-    },
-    {
-        "img5":MiPortfolio,
-        "alt":"MyPortfolio"
-    },
-    {
-        "img6":PI_Henry_Videogames,
-        "alt":"PI_Henry_Videogames"
-    }, {
-        "icon_linkedin": linkedin
-    }
-
-]
+  {
+    img1: LiteraryCorner,
+    alt: 'LiteraryCorner',
+  },
+  {
+    img2: YearSale,
+    alt: 'YearSale',
+  },
+  {
+    img3: Homework,
+    alt: 'Homework_list',
+  },
+  {
+    img4: BadBear,
+    alt: 'BadBear',
+  },
+  {
+    img5: MiPortfolio,
+    alt: 'MyPortfolio',
+  },
+  {
+    img6: PI_Henry_Videogames,
+    alt: 'PI_Henry_Videogames',
+  },
+];
