66ab5e5c645df8440e28a534e9770b44ab81bb29
diegohansselperez <diegohansselp24@gmail.com>
2025-09-21 15:05:54 -0700

    Añadimos un useEffect para perpetuar el modo nocturno en la párte mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { AboutMe } from './Aboutme';
 import { Contact } from './Contact';
 import { Footer } from './Footer';
@@ -8,6 +9,20 @@ import StarBackground from './StarBackground';
 import ThemeToggle from './ThemeToggle';
 
 export function Home() {
+  useEffect(() => {
+    handelMobileWindow();
+  }, []);
+
+  const handelMobileWindow = () => {
+    const widthMobileStatus = 640;
+    const windowActual = window.innerWidth;
+
+    if (windowActual < widthMobileStatus) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  };
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
